000200b30ac6c887ff06edb468affe72ccb1888a
Artem <armastra@gmail.com>
2022-09-08 19:17:36 +0700

    Add a line renderers for trace ships traectory

--- Assets/Scripts/PlanetStats.cs
+++ Assets/Scripts/PlanetStats.cs
@@ -10,6 +10,11 @@ public class PlanetStats : MonoBehaviour
     private EShipOwner _owner = EShipOwner.None;
     private SpriteRenderer _first, _second;
     private Text _text;
+    private GameObject _highlight;
+    private LineRenderer _lineRenderer;
+    private Camera _mainCam;
+
+    private bool _inDragMode = false;
 
     public int Ships { set => _ships = value; get => _ships; }
 
@@ -20,6 +25,15 @@ public class PlanetStats : MonoBehaviour
         _first = sprites.Find("First").GetComponent<SpriteRenderer>();
         _second = sprites.Find("Second").GetComponent<SpriteRenderer>();
         _text = transform.Find("Canvas").Find("Ships").GetComponent<Text>();
+        _highlight = transform.Find("Highlight").gameObject;
+        _lineRenderer = transform.Find("LineRenderer").GetComponent<LineRenderer>();
+        _mainCam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        // set line position to zero (center of the planet)
+        _lineRenderer.SetPositions(new Vector3[] {
+            new Vector3(transform.position.x, transform.position.y, 0),
+            new Vector3(transform.position.x, transform.position.y, 0)
+        });
+
         InvokeRepeating(nameof(UpdateShip), 1, 1);
     }
 
@@ -50,4 +64,48 @@ public class PlanetStats : MonoBehaviour
             _second.color = Resources.instance.ComputerColor;
         }
     }
+
+    // set planet highlighted
+    private void OnMouseEnter()
+    {
+        _highlight.SetActive(true);
+    }
+
+    // set planet not highlighted
+    private void OnMouseExit()
+    {
+        if (!_inDragMode)
+            _highlight.SetActive(false);
+    }
+
+    // start unit drag
+    private void OnMouseDrag()
+    {
+        if (_owner != EShipOwner.Player)
+        {
+            return;
+        }
+        _inDragMode = true;
+        Vector3 pos = _mainCam.ScreenToWorldPoint(Input.mousePosition);
+        _lineRenderer.SetPosition(1, new Vector3(pos.x, pos.y, 0));
+    }
+
+    //end unit drag - if stopped on planet - send units to destination
+    private void OnMouseUp()
+    {
+        _inDragMode = false;
+        OnMouseExit();
+        // if not the same planet
+        if(_lineRenderer.GetPosition(0) != _lineRenderer.GetPosition(1))
+        {
+            // and if on the planet
+        }
+        _lineRenderer.SetPosition(1, _lineRenderer.GetPosition(0));
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        _lineRenderer.SetPosition(1, _lineRenderer.GetPosition(0));
+    }
+
 }
